69a5315c78077416b5327832e06e7b057de088ed
Ndumiso-Y <ndumisoyedwa@gmail.com>
2025-09-02 12:14:19 +0200

    Test: Absolute minimal React setup with no JSX, no CSS, no external files

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-console.log('React app starting with CSS...')
+console.log('Starting minimal React app...')
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App() {
+  return React.createElement('div', {
+    style: { padding: '20px', color: 'white', backgroundColor: 'black' }
+  }, 
+    React.createElement('h1', null, 'MINIMAL REACT TEST'),
+    React.createElement('p', null, 'If you see this, React works!'),
+    React.createElement('button', {
+      onClick: () => console.log('Button clicked!')
+    }, 'Click me')
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(App())
