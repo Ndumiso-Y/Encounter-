f02b6a1e4a574dd5f6c8a637a4cc0d4ff2ded215
Ndumiso-Y <ndumisoyedwa@gmail.com>
2025-09-02 12:09:00 +0200

    Test with simplified Tailwind config and CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-console.log('React app starting...')
+console.log('React app starting with CSS...')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
